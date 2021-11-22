a247b343dcedf2602b2871ad8c0d4fb68814d6aa
Teddy Jean-Francois <teddy.jean-francois@ibm.com>
2021-11-22 14:12:20 +0100

    feat : contract initialization

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
 var MultiSigWallet = artifacts.require("./MultiSigWallet.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(MultiSigWallet);
+module.exports = async (deployer) => {
+  const accounts = await web3.eth.getAccounts();
+
+  const addrAccount1 = accounts[0];
+  const addrAccount2 = accounts[1];
+  const addrAccount3 = accounts[2];
+
+  const owners = [ addrAccount1, addrAccount2, addrAccount3];
+  const numConfirmationsRequired = 3;
+
+  await deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,56 @@
+require('dotenv').config()
+
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider"); 
+
+const private_keys = [
+  process.env.ACCOUNT_1,
+  process.env.ACCOUNT_2,
+  process.env.ACCOUNT_3,
+  process.env.ACCOUNT_4
+];
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        privateKeys: private_keys,
+        providerOrUrl: `https://rinkeby.infura.io/v3/${ process.env.INFURA_API_KEY }`,
+        numberOfAddresses: 4
+      }), 
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     },
   },
-    compilers: {
-      solc: {
-        version: "^0.8.0"
+
+  compilers: {
+    solc: {
+      version: "0.8.3",
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
       }
+    }
+  },
+
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
   }
+
 };
